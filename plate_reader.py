c3e22f42ba3f4bcfc80feae5e606d87d20778615
duc-1001 <126683051+duc-1001@users.noreply.github.com>
2025-05-03 23:32:18 +0700

    Update code

--- plate_reader.py
+++ plate_reader.py
@@ -29,6 +29,49 @@ def detect_plate_adaptive(img):
                                   cv2.THRESH_BINARY_INV, 11, 2)
     return thresh
 
+def order_points(pts):
+    # Sắp xếp 4 điểm theo thứ tự: top-left, top-right, bottom-right, bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+    
+    # Điểm có tổng tọa độ nhỏ nhất là top-left, lớn nhất là bottom-right
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    
+    # Điểm có hiệu tọa độ nhỏ nhất là top-right, lớn nhất là bottom-left
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    
+    return rect
+
+def four_point_transform(image, pts):
+    # Lấy các điểm và tính toán kích thước của ảnh kết quả
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    
+    # Tính chiều rộng mới (max của khoảng cách giữa bottom-right và bottom-left hoặc top-right và top-left)
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    
+    # Tính chiều cao mới (max của khoảng cách giữa top-right và bottom-right hoặc top-left và bottom-left)
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    
+    # Tạo ma trận đích và tính ma trận biến đổi phối cảnh
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    
+    return warped
+
 def find_plate_contours(edged, original_img):
     # Tìm contours và lọc theo diện tích và tỷ lệ khung hình
     contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -53,7 +96,7 @@ def find_plate_contours(edged, original_img):
 
 def main():
     # Đọc ảnh và thay đổi kích thước
-    img = cv2.imread('image1.jpg')
+    img = cv2.imread('cach-lap-bien-so-xe-o-to-chac-chan-khong-roi-r6o8ilr.jpg')
     if img is None:
         print("Không thể đọc ảnh")
         return
@@ -83,13 +126,13 @@ def main():
         # Vẽ contour lên ảnh gốc
         cv2.drawContours(img, [plate_contour], -1, (0, 255, 0), 3)
         
-        # Cắt phần biển số
-        (x, y, w, h) = cv2.boundingRect(plate_contour)
-        number_plate = img[y:y+h, x:x+w]
+        # Reshape contour và áp dụng phép biến đổi phối cảnh
+        pts = plate_contour.reshape(4, 2)
+        warped = four_point_transform(img, pts)
         
         # Hiển thị kết quả
         cv2.imshow("Detected Plate", img)
-        cv2.imshow("Extracted Number Plate", number_plate)
+        cv2.imshow("Corrected Number Plate", warped)
         cv2.waitKey(0)
     
     cv2.destroyAllWindows()
